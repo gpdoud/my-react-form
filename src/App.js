e44601ca3eabe7e5d4e03e8a8bdf62656dea35bf
Greg Doud <gpdoud@gmail.com>
2022-11-19 09:11:59 -0500

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [ inputs, setInputs ] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputs);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+        <label>Name:
+          <input type="text" 
+                  name="username" 
+                  value={inputs.username || ""} 
+                  onChange={handleChange} />
+        </label>
+        <label>Age:
+          <input type="number" 
+                  name="age" 
+                  value={inputs.age || ""} 
+                  onChange={handleChange} />
+        </label>
+        <label>Amount:
+          <input type="number" 
+                  name="amount" 
+                  value={inputs.amount || 0} 
+                  onChange={handleChange} />
+        </label>
+        <input type="submit" />
+    </form>
   );
+  
 }
+
 
 export default App;
